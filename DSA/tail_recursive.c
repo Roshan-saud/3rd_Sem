d6c65e64c98348fec0658a483cb3ea55eacad867
Roshan-saud <saudroshan2739@gmail.com>
2025-04-10 19:18:32 +0545

    tail recursion add

--- DSA/tail_recursive.c
+++ DSA/tail_recursive.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-void tail(int n)
-{
-    while (n > 0)
-    {
-        printf("%d,", n);
-        n--;
-    }
+// Tail-recursive function to print from n to 0
+void Desc(int n) {
+    if (n < 0)
+    return;
+    printf("%d ", n);
+    Desc(n - 1);  // Tail-recursive call
 }
 
-int main()
-{
+int main() {
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    tail(n);
-    return 0;
+
+    printf("Numbers in decreasing order:\n");
+    Desc(n);
 }
